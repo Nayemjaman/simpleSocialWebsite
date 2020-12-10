e4ef8f24440ca68583ed4ec68967bdce9adb9306
Nayem Jaman Tusher <njaman26@gmail.com>
2020-12-11 03:12:23 +0600

    ready for media

--- account/forms.py
+++ account/forms.py
@@ -8,7 +8,7 @@ class LoginForm(forms.Form):
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    password = forms.CharField(label='password', widget=forms.PasswordInput, )
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
@@ -17,6 +17,7 @@ class UserRegistrationForm(forms.ModelForm):
 
     def clean_password2(self):
         cd = self.cleaned_data
+        print("cd... : ", cd)
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('password don\'t match')
         return cd['password2']
--- account/views.py
+++ account/views.py
@@ -37,8 +37,10 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
+            # temp user
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
+            # ... finally save the user
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
