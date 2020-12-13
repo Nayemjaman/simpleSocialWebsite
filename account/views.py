6d2ea6d4c53b562d25b179d778cc6415474d74b5
Nayem Jaman Tusher <njaman26@gmail.com>
2020-12-13 16:13:27 +0600

    install social auth pkz

--- account/views.py
+++ account/views.py
@@ -58,7 +58,7 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated '\
+            messages.success(request, 'Profile updated '
                                       'successfully')
         else:
             messages.error(request, 'Error updating your profile')
